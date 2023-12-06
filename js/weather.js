ad701aa8b9c5ba6ca43ed82deb512ec72b8f7222
Dalemar Glenn <dale_197@outlook.com>
2023-12-06 15:19:29 -0500

    feat: crate card on dropdown click

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 let citiesDropDown = document.querySelector("#citiesDropDown");
-let weatherDiv = document.querySelector("#weatherDiv")
+let weatherDiv = document.querySelector("#weatherDiv");
 
 let cities = [
    {
@@ -40,29 +40,91 @@ let cities = [
 ];
 
 document.onload = init();
-citiesDropDown.onchange = () => getWeatherStation(citiesDropDown.value)
+citiesDropDown.onchange = () => getWeatherStation(citiesDropDown.value);
 
 function init() {
-    for (const city of cities) {
-        let option = new Option(city.name, city.latitude + "," + city.longitude)
-        citiesDropDown.appendChild(option)
-    }
+   for (const city of cities) {
+      let option = new Option(city.name, city.latitude + "," + city.longitude);
+      citiesDropDown.appendChild(option);
+   }
 }
 
 async function getWeatherStation(coordinates) {
-    let responce = await fetch(`https://api.weather.gov/points/${coordinates}`);
-    let data = await responce.json();
-    let properties = data.properties
-    getForcast(properties.gridId, properties.gridX, properties.gridY);
+   let responce = await fetch(`https://api.weather.gov/points/${coordinates}`);
+   let data = await responce.json();
+   let properties = data.properties;
+   getForcast(properties.gridId, properties.gridX, properties.gridY);
 }
 
 async function getForcast(office, gridX, gridY) {
-    let responce = await fetch(`https://api.weather.gov/gridpoints/${office}/${gridX},${gridY}/forecast`);
-    let data = await responce.json();
-    let weeklyForcast = data.properties.periods
-    console.log(weeklyForcast);
+   let responce = await fetch(`https://api.weather.gov/gridpoints/${office}/${gridX},${gridY}/forecast`);
+   let data = await responce.json();
+   let weeklyForcast = data.properties.periods[0];
+   createCard(
+      weeklyForcast.name,
+      weeklyForcast.temperature,
+      weeklyForcast.windSpeed,
+      weeklyForcast.windDirection,
+      weeklyForcast.dewpoint.value,
+      weeklyForcast.relativeHumidity.value,
+      weeklyForcast.probabilityOfPrecipitation.value,
+      weeklyForcast.shortForecast
+   );
 }
 
-function createCard() {
+function createCard(
+   name,
+   tempature,
+   windSpeed,
+   windDirection,
+   dewPoint,
+   humidity,
+   pOP,
+   shortForecast,
+   detailedForecast
+) {
+   let card = document.createElement("div");
+   card.className = "card";
+
+   let cardTitle = document.createElement("h5");
+   cardTitle.innerText = name;
+   cardTitle.className = "card-title";
+
+   let cardBody = document.createElement("div");
+   cardBody.className = "card-body";
+
+   let daysTempature = document.createElement("h3");
+   daysTempature.innerText = `${tempature} °F`;
+   daysTempature.className = "card-text";
+
+   let wind = document.createElement("p");
+   wind.innerText = `Wind: ${windSpeed} from ${windDirection}`;
+   wind.className = "card-text";
+
+   let dewNHumidity = document.createElement("p");
+   dewNHumidity.innerText = `Dewpoint: ${dewPoint.toFixed(2)}°C | Humidity: ${humidity}%`;
+   dewNHumidity.className = "card-text";
+
+   let percipitation = document.createElement("p");
+   percipitation.innerText = `${pOP}% chance of percipitation`;
+   percipitation.className = "card-text";
+
+   let briefForecast = document.createElement("p");
+   briefForecast.innerText = shortForecast;
+   briefForecast.className = "card-text";
+
+   cardBody.appendChild(cardTitle);
+   cardBody.appendChild(daysTempature);
+   cardBody.appendChild(wind);
+   cardBody.appendChild(dewNHumidity);
+   
+    if (pOP) {
+        cardBody.appendChild(percipitation);
+    }
     
+   cardBody.appendChild(briefForecast);
+
+   card.appendChild(cardBody);
+
+   weatherDiv.appendChild(card);
 }
