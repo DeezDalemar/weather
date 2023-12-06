f327cab0c5547ee9e35239f9f9e3fa5a1dbc6f9b
Dalemar Glenn <dale_197@outlook.com>
2023-12-06 10:40:16 -0500

    feat: index init, add cities array

---
+++ js/weather.js
@@ -0,0 +1,37 @@
+let cities = [
+   {
+      name: "Pittsburgh, PA",
+      latitude: 40.440624,
+      longitude: -79.995888,
+   },
+   {
+      name: "Philadelphia, PA",
+      latitude: 39.9526,
+      longitude: -75.1652,
+   },
+   {
+      name: "Harrisburg, PA",
+      latitude: 40.273191,
+      longitude: -76.8867,
+   },
+   {
+      name: "Allentown, PA",
+      latitude: 40.602206,
+      longitude: -75.471394,
+   },
+   {
+      name: "Scranton, PA",
+      latitude: 41.408969,
+      longitude: -75.662412,
+   },
+   {
+      name: "Erie, PA",
+      latitude: 42.129224,
+      longitude: -80.085059,
+   },
+   {
+      name: "Baltimore, MD",
+      latitude: 39.2904,
+      longitude: -76.6122,
+   },
+];
